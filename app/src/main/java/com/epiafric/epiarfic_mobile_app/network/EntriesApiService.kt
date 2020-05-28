28b5ba6827ede29432a97bfd91f4352a1a0bdd6a
Dablaze-ufc <chukwuwauchenna@gmail.com>
2020-05-28 01:45:34 +0100

    initial project setup

--- app/src/main/java/com/epiafric/epiarfic_mobile_app/network/EntriesApiService.kt
+++ app/src/main/java/com/epiafric/epiarfic_mobile_app/network/EntriesApiService.kt
@@ -1,4 +1,8 @@
 package com.epiafric.epiarfic_mobile_app.network
 
+import retrofit2.http.GET
+
 interface EntriesApiService {
+    @GET
+    fun getRecent(){}
 }
--- app/src/main/java/com/epiafric/epiarfic_mobile_app/network/RetrofitBuilder.kt
+++ app/src/main/java/com/epiafric/epiarfic_mobile_app/network/RetrofitBuilder.kt
@@ -6,7 +6,7 @@ import com.squareup.moshi.kotlin.reflect.KotlinJsonAdapterFactory
 import retrofit2.Retrofit
 import retrofit2.converter.moshi.MoshiConverterFactory
 
-private const val BASE_URL = "https://epiafric.herokuapp.com/api/v1"
+private const val BASE_URL = "https://epiafric.herokuapp.com/api/v1/reports"
 
 private val moshi = Moshi.Builder().add(KotlinJsonAdapterFactory()).build()
 
