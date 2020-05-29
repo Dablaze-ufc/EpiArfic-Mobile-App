912c390afe34dadfde79182638fae73173d839a2
Dablaze-ufc <chukwuwauchenna@gmail.com>
2020-05-29 13:40:37 +0100

    resolved recyclerview errors

--- app/src/main/java/com/epiafric/epiarfic_mobile_app/adapter/EntriesDataAdapter.kt
+++ app/src/main/java/com/epiafric/epiarfic_mobile_app/adapter/EntriesDataAdapter.kt
@@ -1,28 +1,26 @@
 package com.epiafric.epiarfic_mobile_app.adapter
 
 import android.view.LayoutInflater
+import android.view.View
 import android.view.ViewGroup
 import androidx.recyclerview.widget.DiffUtil
 import androidx.recyclerview.widget.ListAdapter
 import androidx.recyclerview.widget.RecyclerView
-import com.epiafric.epiarfic_mobile_app.databinding.RecentListItemBinding
+import com.epiafric.epiarfic_mobile_app.R
 import com.epiafric.epiarfic_mobile_app.model.Data
+import kotlinx.android.synthetic.main.recent_list_item.view.*
 
 class EntriesDataAdapter: ListAdapter<Data, EntriesDataAdapter.EntriesViewHolder>(DiffCallBack) {
     companion object DiffCallBack: DiffUtil.ItemCallback<Data>(){
         override fun areItemsTheSame(oldItem: Data, newItem: Data): Boolean {
             return oldItem === newItem
         }
-
         override fun areContentsTheSame(oldItem: Data, newItem: Data): Boolean {
             return oldItem.id == newItem.id
         }
-
     }
-
-
     override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): EntriesViewHolder {
-        return EntriesViewHolder(RecentListItemBinding.inflate(LayoutInflater.from(parent.context)))
+        return EntriesViewHolder.from(parent)
     }
 
     override fun onBindViewHolder(holder: EntriesViewHolder, position: Int) {
@@ -31,13 +29,21 @@ class EntriesDataAdapter: ListAdapter<Data, EntriesDataAdapter.EntriesViewHolder
 
     }
 
-    class EntriesViewHolder(private var binding : RecentListItemBinding) :RecyclerView.ViewHolder(binding.root){
-        fun bind(entriesData: Data){
-            binding.entriesData = entriesData
-            binding.executePendingBindings()
+    class EntriesViewHolder(itemView: View) :RecyclerView.ViewHolder(itemView) {
+
+        fun bind(entriesData: Data) = with(itemView) {
+
+            itemView.text_tittle.text = entriesData.date_created
+
+            itemView.text_details.text = entriesData.date_of_entry
         }
-
+        
+        companion object {
+            fun from(parent: ViewGroup): EntriesViewHolder {
+                val layoutInflater = LayoutInflater.from(parent.context)
+                val binding = layoutInflater.inflate(R.layout.recent_list_item, parent, false)
+                return EntriesViewHolder(binding)
+            }
+        }
     }
-
-
 }
