5b070a2f3e7afb23631cc2ffc60ae1b8f1be6c97
Dablaze-ufc <chukwuwauchenna@gmail.com>
2020-05-30 15:31:58 +0100

    code refactor to the retrofit call

--- app/src/main/java/com/epiafric/epiarfic_mobile_app/network/RetrofitBuilder.kt
+++ app/src/main/java/com/epiafric/epiarfic_mobile_app/network/RetrofitBuilder.kt
@@ -3,6 +3,8 @@ package com.epiafric.epiarfic_mobile_app.network
 import com.jakewharton.retrofit2.adapter.kotlin.coroutines.CoroutineCallAdapterFactory
 import com.squareup.moshi.Moshi
 import com.squareup.moshi.kotlin.reflect.KotlinJsonAdapterFactory
+import okhttp3.OkHttpClient
+import okhttp3.logging.HttpLoggingInterceptor
 import retrofit2.Retrofit
 import retrofit2.converter.moshi.MoshiConverterFactory
 
@@ -10,10 +12,12 @@ private const val BASE_URL = "https://epiafric.herokuapp.com/api/v1/"
 
 private val moshi = Moshi.Builder().add(KotlinJsonAdapterFactory()).build()
 
+private val okHttpClient = OkHttpClient.Builder().addInterceptor(HttpLoggingInterceptor().setLevel(HttpLoggingInterceptor.Level.BODY)).build()
+
 object EntriesApi{
     private val retrofit =
         Retrofit.Builder().addCallAdapterFactory(CoroutineCallAdapterFactory()).addConverterFactory(MoshiConverterFactory.create(
-            moshi)).baseUrl(BASE_URL).build()
+            moshi)).client(okHttpClient).baseUrl(BASE_URL).build()
 
     val retrofitService  : EntriesApiService by lazy {
         retrofit.create(EntriesApiService::class.java)
--- app/src/main/java/com/epiafric/epiarfic_mobile_app/repository/EpiAfricRepository.kt
+++ app/src/main/java/com/epiafric/epiarfic_mobile_app/repository/EpiAfricRepository.kt
@@ -17,7 +17,7 @@ class EpiAfricRepository (private val api:EntriesApiService, private val dao: En
 
      suspend fun getRecentFromApi() {
        withContext(Dispatchers.IO){
-           val entries = api.getRecentFromApi().await()
+           val entries = api.getRecentFromApi(300).await()
           dao.setRecentEntries(entries.data)
        }
     }
